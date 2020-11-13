ebb6a7d03cfe5aa10cb87187a6f23c8e1c6266ff
alex.g <aleks.g@digital-spectr.ru>
2020-11-13 14:48:15 +0500

    dev| /var/www/vue/ "mkdir lol"

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
             }
         }
     },
-    publicPath: '/vue/dist/',
+    publicPath: '/vue/',
     chainWebpack: config => {
         config.plugin('html').tap((args) => {
             args[0].minify = {
